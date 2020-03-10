6bec8df8b6637c44b69355d24911562b711bd027
Katherine-Kelly <61455651+Katherine-Kelly@users.noreply.github.com>
2020-03-10 10:47:34 +0100

    9-03-2020
    Survival plots created based on selected protein from TCGA Breast dataset. Short description of analysis displayed below graph.

--- server.R
+++ server.R
@@ -3,7 +3,7 @@ library(survival)
 library(survminer)
 library(ggplot2)
 
-BRCA <- read.csv('BRCA_subset_median.csv')
+BRCA <- read.csv('BRCA_survival_median.csv')
 surv_object <- Surv(time = BRCA$DSS.time, event = BRCA$DSS)
 
 shinyServer(function(input, output) {
@@ -13,6 +13,14 @@ shinyServer(function(input, output) {
         paste("Kaplan Meier Survival Graph of", input$Protein_ID, sep="\n")
     })
     
+    output$info <- renderText({
+        paste("Red line indicates high expression, and blue line indicates low expression of", input$Protein_ID, ".")
+    })
+    
+    output$moreinfo <- renderText({
+        paste("Data is separated based on median expression from 873 patients in the TCGA Breast dataset.")
+    })
+    
     # Running the survival function
     runSur <- reactive({
         survfit(as.formula(paste("Surv(DSS.time,DSS) ~ ",paste(input$Protein_ID))),data=BRCA)
@@ -20,9 +28,16 @@ shinyServer(function(input, output) {
     })
     
     output$plot_survival <- renderPlot({
-        plot(runSur(), col=c('red', 'blue'), xlab='Time (weeks)', ylab='Probability of Survival', pch=5)
+        plot(runSur(), col=c('red', 'blue'), xlab='Time (weeks)', ylab='Probability of Survival', lwd=2) 
+        legend(10,10, legend=c("High", "Low"),
+               col=c("red", "blue"))
+    
     })
     
+    
+    summary <- reactive({
+        summary(runSur())
+    })
     
     output$summary <- renderPrint({
         surv_object <- Surv(time = BRCA$OS.time, event = BRCA$OS)
@@ -30,8 +45,11 @@ shinyServer(function(input, output) {
         summary(fit)
     })
     
+
+    
     output$raw_data <- renderTable({
         BRCA
     }, include.rownames = FALSE)
     
 })
+
--- ui.R
+++ ui.R
@@ -1,9 +1,7 @@
 library(shiny)
 
-BRCA <- read.csv('BRCA_subset_median.csv')
+BRCA <- read.csv('BRCA_survival_median.csv')
 surv_object <- Surv(time = BRCA$DSS.time, event = BRCA$DSS)
-
-proteins <- as.vector(colnames(BRCA[4:6]))
 
 # Define UI for dataset viewer application
 shinyUI(fluidPage(theme = "bootstrap.min.css",
@@ -16,16 +14,20 @@ shinyUI(fluidPage(theme = "bootstrap.min.css",
                       sidebarPanel(
                           selectInput(inputId = "Protein_ID",
                                       'Select Protein ID',
-                                      names(BRCA)[names(BRCA) != "patient" & names(BRCA) != "DSS" & names(BRCA) != "DSS.time"]
-                          )
+                                      names(BRCA[16:236])
+                          ),
+                          selectInput(inputId = "subtype",
+                                      'Select cancer subtype (optional)',
+                                      na.omit(BRCA$subtype))
                       ), 
                       
                       mainPanel(
                           tabsetPanel(
-                              tabPanel("Survival Plot", plotOutput("plot_survival"), h2(textOutput("caption"))),
+                              tabPanel("Survival Plot", plotOutput("plot_survival"), h2(textOutput("caption"), h4(textOutput("info")), h4(textOutput("moreinfo")))),
                               tabPanel("Data Summary", verbatimTextOutput("summary")),
                               tabPanel("Data", tableOutput("raw_data")),
                               id = "tabs"
                           ) 
                       )
                   )))
+
