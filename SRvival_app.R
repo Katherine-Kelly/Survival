0fc6c2c5c8cd40477c401903f27e9c1f061fe768
Katherine-Kelly <61455651+Katherine-Kelly@users.noreply.github.com>
2020-03-25 14:25:13 +0100

    Final Version
    Final changes made: Added stacked barplots to summary section and changed "protein id" to "variable id" in custom data section.

--- SRvival_app.R
+++ SRvival_app.R
@@ -38,7 +38,7 @@ ui <- fluidPage(
                  tabsetPanel(
                      tabPanel("Survival Summary", fluidRow(splitLayout(cellWidths = c("50%", "50%"), plotOutput("subtypeKM"), plotOutput("stageKM")))),
                      tabPanel("COX Proportional Hazard Models", fluidRow(splitLayout(cellWidths = c("33%", "33%", "33%"), plotOutput("ageCox"), plotOutput("subtypeCox"), plotOutput("stageCox")))),
-                              tabPanel("Distribution of Data", fluidRow(splitLayout(cellWidths = c("33%", "33%", "33%"),plotOutput("subtypesummary"), plotOutput("agesummary"), plotOutput("gradesummary"))),
+                              tabPanel("Distribution of Data", fluidRow(splitLayout(cellWidths = c("33%", "33%", "33%"),plotOutput("subtypesummary"), plotOutput("agesummary"), plotOutput("gradesummary"))), fluidRow(splitLayout(cellWidths = c("50%", "50%"), plotOutput("stacked1"), plotOutput("stacked2"))),
                      )
                  )
                  
@@ -46,8 +46,8 @@ ui <- fluidPage(
         tabPanel("Custom Data",
                  sidebarLayout(   
                      sidebarPanel(width = 3,
-                         textInput(inputId = "Protein_ID2",
-                                   'Enter Protein ID'),
+                         textInput(inputId = "custom_variable",
+                                   'Enter Variable Name'),
    
                          fileInput("file1", "Upload CSV File",
                                    multiple = FALSE,
@@ -144,19 +144,29 @@ server <- function(input, output) {
     # Summary Plots #
     #################
     
+    
+    # Barplots by subtype, age group and tumour stage
     output$subtypesummary <- renderPlot({
         plot(BRCA$subtype, col='steelblue', border=F, main= "Distribution of data by subtype")
-        
     })
     output$agesummary <- renderPlot({
         plot(BRCA$age, col='steelblue', border=F, main= "Distribution of data by age")
-        
     })
     output$gradesummary <- renderPlot({
         plot(BRCA$ajcc_pathologic_tumor_stage, col='steelblue', border=F, main= "Distribution of data by tumor stage")
-        
     })
     
+    
+    # Stacked barplots of subtype by age group and tumour stage
+    output$stacked1 <- renderPlot({
+        plot(BRCA$subtype, BRCA$age, col=c("black", "darkblue", "dodgerblue3", "cornflowerblue", "lightskyblue", "lightblue", "white"), main="Subtype distribution by age group", xlab="Subtype", ylab="Age group")
+    })
+    output$stacked2 <- renderPlot({
+        plot(BRCA$subtype, BRCA$ajcc_pathologic_tumor_stage, col=c("darkblue", "cornflowerblue", "lightblue", "white"), main="Subtype distribution by tumour stage", xlab="Subtype", ylab="AJCC Pathological Tumour Stage")
+    })
+    
+    
+    # KM survival plots by subtype and stage
     output$subtypeKM <- renderPlot({
         surv_object <- Surv(time = BRCA$OS.time, event = BRCA$OS)
         BRCA$subtype <- factor(BRCA$subtype, levels = c("Basal", "LumA", "LumB", "Her2"))
@@ -164,8 +174,6 @@ server <- function(input, output) {
         plot(fit, col=c("blue", "purple", "turquoise", "seagreen"), xlab='Time (days)', ylab='Probability of Survival', lwd=2, main=c(paste("Kaplan Meier Plot of Suvival by Subtype"))) 
         legend(0,0.4, legend=c("Basal", "LumA", "LumB", "Her2"), col=c("blue", "purple", "turquoise", "seagreen"),lty=1, lwd=4)
     })
-    
-    
     output$stageKM <- renderPlot({
         surv_object <- Surv(time = BRCA$OS.time, event = BRCA$OS)
         fit <- survfit(surv_object ~ ajcc_pathologic_tumor_stage, data=BRCA)
@@ -173,22 +181,18 @@ server <- function(input, output) {
         legend(0,0.3, legend=c("Stage I", "Stage II", "Stage III", "Stage IV"), col=c("blue", "purple", "turquoise", "seagreen"),lty=1, lwd=4)
     })
     
-    output$subtypeCox <- renderPlot({
-        BRCA$subtype <- factor(BRCA$subtype, levels = c("Basal", "LumA", "LumB", "Her2"))
-        fit.coxph <- coxph(Surv(time = BRCA$OS.time, event = BRCA$OS) ~ subtype, data = BRCA)
-        ggforest(fit.coxph, main="Hazard Ratio by Subtype", data=BRCA)
-        
-    })
     
+    # Forest plots illustrate survival by subtype, stage, age group, as analysed by cox proportional hazard models
+    output$subtypeCox <- renderPlot({
+        BRCA$subtype <- factor(BRCA$subtype, levels = c("Normal", "Basal", "LumA", "LumB", "Her2"))
+        fit.coxph <- coxph(Surv(time = BRCA$OS.time, event = BRCA$OS) ~ subtype, data = BRCA)
+        ggforest(fit.coxph, main="Hazard Ratio by Subtype", data=BRCA)   
+    })
     output$ageCox <- renderPlot({
-        
         fit.coxph <- coxph(Surv(time = BRCA$OS.time, event = BRCA$OS) ~ age, data = BRCA)
         ggforest(fit.coxph,main="Hazard Ratio by Age Group", data=BRCA)
-        
     })
-    
     output$stageCox <- renderPlot({
-        
         fit.coxph <- coxph(Surv(time = BRCA$OS.time, event = BRCA$OS) ~ ajcc_pathologic_tumor_stage, data = BRCA)
         ggforest(fit.coxph, main="Hazard Ratio by Tumour Stage", data=BRCA)
     })
@@ -197,6 +201,7 @@ server <- function(input, output) {
     ###################
     #   Custom Data   #
     ###################
+    
     
     # Define dataframe from input file
     myData <- reactive ({
@@ -208,14 +213,14 @@ server <- function(input, output) {
     
     # Run survival function on input data
     runSur2 <- reactive({
-        req(input$Protein_ID2)
-        survfit(as.formula(paste("Surv(time,endpoint) ~ ",paste(input$Protein_ID2))),data=myData())
+        req(input$custom_variable)
+        survfit(as.formula(paste("Surv(time,endpoint) ~ ",paste(input$custom_variable))),data=myData())
         
     })
     
     # Plot survival function
     output$plot_survival2 <- renderPlot ({
-        plot(runSur2(), col=c('red', 'blue'), xlab='Time (days)', ylab='Probability of Survival', lwd=2, main = c(paste("Kaplan Meier Survival Plot of",paste(input$Protein_ID2))))
+        plot(runSur2(), col=c('red', 'blue'), xlab='Time (days)', ylab='Probability of Survival', lwd=2, main = c(paste("Kaplan Meier Survival Plot of",paste(input$custom_variable))))
         legend(0,0.2, legend=c("High expression", "Low expression"), col=c("red", "blue"),lty=1, lwd=4)
         
     })
@@ -228,8 +233,8 @@ server <- function(input, output) {
     
     # Test for difference in survival curves
     diff2 <- reactive ({
-        req(input$Protein_ID2)
-        diff <- survdiff(as.formula(paste("Surv(time,endpoint) ~" , input$Protein_ID2)), data = myData())
+        req(input$custom_variable)
+        diff <- survdiff(as.formula(paste("Surv(time,endpoint) ~" , input$custom_variable)), data = myData())
         return(diff)
     })
     
@@ -238,7 +243,7 @@ server <- function(input, output) {
     })
     
     
-    # Print sample data table structure for "Help section"
+    # Print sample data table structure for "Help" section
     output$sampletable <- renderTable({
         sample <- read.csv("BRCA_subset_median.csv")
         head(sample)
